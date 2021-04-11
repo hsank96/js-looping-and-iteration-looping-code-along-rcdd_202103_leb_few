a9e899974e116efc3b8dc4fcac6a2111c67f0999
Hazem El Sankari <hazemleb96@gmail.com>
2021-04-11 07:42:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 //let names =[ 'Lisa', 'Kaitlin', 'Jan' ];
 //let events = 'surprise';
-let messages=[];
 
 function writeCards(names,events){
+  let messages=[];
   for (let i=0; i<names.length; i++) {
     messages.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
   }
